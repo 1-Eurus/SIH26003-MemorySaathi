db04159c32f48ec780c8b2e6661cf37ae09f66c3
Shloak Gupta <shloak.gupta.e@gmail.com>
2026-09-06 00:12:28 +0530

    android build try 6, fixed capctior implementation

---
+++ android/app/src/main/java/com/eurus/memorysaathi/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eurus.memorysaathi;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eurus.memorysaathi',
+  appName: '\x16\x16\x16MemorySaathi',
+  webDir: 'dist'
+};
+
+export default config;
