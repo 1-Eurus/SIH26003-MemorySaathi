d780a17fc4af41832707f4f30466fe26e3ad7fa8
Shloak Gupta <shloak.gupta.e@gmail.com>
2026-09-06 06:55:30 +0530

    allowed overflow scroll down

--- src/App.tsx
+++ src/App.tsx
@@ -3066,7 +3066,7 @@ export default function App() {
               />
             ) : (
               <>
-                <div className="flex-1 overflow-hidden">
+                <div className="flex-1 min-h-0 overflow-y-auto pb-24">
                   {screen === 'home' && (
                     <HomeScreen
                       onNavigate={navigate}
